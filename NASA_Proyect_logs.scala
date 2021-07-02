8f176fee8b10dd32186eb8f42adb7adb133b9d2a
JohnIbarraBosonit <85739680+JohnIbarraBosonit@users.noreply.github.com>
2021-07-02 10:30:05 +0200

    Update NASA_Proyect_logs.scala

--- NASA_Proyect_logs.scala
+++ NASA_Proyect_logs.scala
@@ -12,6 +12,7 @@ NasaLog_CSV.show()
 
 // COMMAND ----------
 
+//Creamos una expresion regular por cada campo a extraer
 val host="""(\S+)"""
 val userIdentifier="""\s(-|\S+)"""
 val userId="""\s(-|\S+)"""
@@ -23,12 +24,7 @@ val size="""(\d+)$"""
 
 // COMMAND ----------
 
-val nasa_df2 = NasaLog_CSV
-  .select(regexp_extract($"value", host,1).alias("host"))
-nasa_df2.show(false)
-
-// COMMAND ----------
-
+//Cargamos nuestro dataframe
 val nasa_df = NasaLog_CSV
   .select(regexp_extract($"value", host,1).alias("host"),
           regexp_extract($"value", userIdentifier, 1).alias("user"),
@@ -43,10 +39,13 @@ val nasa_df = NasaLog_CSV
 
 // COMMAND ----------
 
+//Descartamos algunos campos que no son necesarios
 val dfSave = nasa_df.select("host", "date", "method", "resource", "protocol", "status", "size")
 dfSave.show()
 
 // COMMAND ----------
+
+//Realizamos el cambio de formato del campo fecha y sustituimos campos vacíos (en size por ceros) y (en method por OTHER)
 
 val df_nasa_sinBlancos = dfSave
 .withColumn("date", to_timestamp(col("date"), "dd/MMM/yyyy:HH:mm:ss"))
@@ -57,6 +56,8 @@ df_nasa_sinBlancos.select("*").orderBy("size").show()
 
 // COMMAND ----------
 
+/*Guardaremos nuestro nuevo DataFrame ya estructurado en formato parquet. Y de este 
+leeremos para realizar nuestro análisis*/
 df_nasa_sinBlancos.write.format("parquet").mode("overwrite").save("/NasaLogTratado")
 
 // COMMAND ----------
@@ -66,43 +67,46 @@ df_nasa.printSchema()
 
 // COMMAND ----------
 
+//He creado una vista temporal y cambie el tipo de datos de algunos campos
 df_nasa.createOrReplaceTempView("logs")
 val df_Cast = spark.sql("SELECT host, date, method, resource, protocol, CAST(status AS INT), CAST(size AS INT) FROM logs")
 
 // COMMAND ----------
-
+//¿Cuáles son los distintos protocolos web utilizados? Agrúpalos
 df_Cast.select("protocol").distinct.show()
 
 // COMMAND ----------
-
+/*¿Cuáles son los códigos de estado más comunes en la web? Agrúpalos y ordénalos 
+para ver cuál es el más común*/
 df_Cast.groupBy("status").agg(count("status").alias("cd_estado_mas_comunes")).sort(desc("cd_estado_mas_comunes")).show()
 
 // COMMAND ----------
-
+//¿Y los métodos de petición (verbos) más utilizados?
 df_Cast.groupBy("method").agg(count("method").alias("metodo_mas_usado")).sort(desc("metodo_mas_usado")).show()
 
 // COMMAND ----------
-
+//¿Qué recurso tuvo la mayor transferencia de bytes de la página web?
 df_Cast.groupBy("resource").agg(max("size").alias("mas_bytes")).sort(desc("mas_bytes")).show()
 
 // COMMAND ----------
-
+/*Además, queremos saber que recurso de nuestra web es el que más tráfico recibe. Es 
+decir, el recurso con más registros en nuestro log.*/
 df_Cast.groupBy("resource").agg(count("resource").alias("mas_trafico")).sort(desc("mas_trafico")).show()
 
 // COMMAND ----------
-
+//¿Qué días la web recibió más tráfico?
 df_Cast.withColumn("dia", dayofmonth(col("date"))).groupBy("dia").agg(max("size").alias("Maximo")).sort(desc("Maximo")).show()
 
 // COMMAND ----------
-
+//¿Cuáles son los hosts son los más frecuentes?
 df_Cast.groupBy("host").agg(count("host").alias("host_mas_frecuente")).sort(desc("host_mas_frecuente")).show()
 
 // COMMAND ----------
-
+//¿A qué horas se produce el mayor número de tráfico en la web?
 df_Cast.withColumn("horas", hour(col("date"))).groupBy("horas").agg(count("horas").alias("mas_trafico")).sort(desc("mas_trafico")).show(29)
 
 // COMMAND ----------
-
+//¿Cuál es el número de errores 404 que ha habido cada día?
 df_Cast.withColumn("dias", dayofmonth(col("date")))
        .filter(col("status")==="404")
        .groupBy("dias")
